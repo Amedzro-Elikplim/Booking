adc87bfb4de0da9e7de13c040ec731ae51823680
fed1k <allamurotovcoder@mail.ru>
2022-11-28 18:26:37 -0500

    building about page

--- screens/About.js
+++ screens/About.js
@@ -1,42 +1,70 @@
 import React from "react";
-import { SafeAreaView, Text, View, StyleSheet } from "react-native";
+import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export const About = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Average cost</Text>
+      <View style={style.subContainer}>
+        <Text style={style.text}>Average cost</Text>
         <Text>$20</Text>
       </View>
 
-      <View>
-        <Text>Hours</Text>
+      <View style={style.subContainer}>
+        <Text style={style.text}>Hours</Text>
         <Text>11am - 6pm</Text>
       </View>
 
-      <View>
-        <Text>Phone</Text>
+      <View style={style.subContainer}>
+        <Text style={style.text}>Phone</Text>
         <Text>(505) 718 0274</Text>
       </View>
 
-      <View>
-        <Text>Location</Text>
+      <View style={style.subContainer}>
+        <Text style={style.text}>Location</Text>
         <Text>1019 mora ave</Text>
       </View>
 
-      <View>
-        <Text>Services</Text>
+      <View style={style.subContainer}>
+        <Text style={style.text}>Services</Text>
         <Text>Dine-in. Takeaway</Text>
       </View>
 
-      <View>
-        <Text>Amenities</Text>
+      <View style={style.subContainer}>
+        <Text style={style.text}>Amenities</Text>
         <Text>Wifi</Text>
       </View>
+
+      <TouchableOpacity style={style.btn}>
+        <Text style={style.btnText}>Book</Text>
+      </TouchableOpacity>
+
     </SafeAreaView>
   )
 };
 
 const style = StyleSheet.create({
-  
+  subContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingRight: 8,
+    paddingLeft: 8,
+    marginTop: 8
+  },  
+  text: {
+    color: "#8F3A0A"
+  },
+
+  btn: {
+    backgroundColor: 'black',
+    borderRadius: 50,
+    marginLeft: 25,
+    marginRight: 25,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  btnText: {
+    color: 'white',
+    textAlign: 'center'
+  }
 })
